44f660eafea70ab76489e3de6aa543e9aeee1b9e
Vadim Zinoviev <stragrus@mail.ru>
2021-08-19 11:58:56 +0300

    edit app

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,12 @@ const button = document.querySelector('.btn');
 
 const popover = document.createElement('div');
 popover.className = 'popover';
-popover.innerHTML = `<h3 class="header-popover">Popover title</h3>
-   <p class="content-popover">And here's some amazing content. It's very engaging. Right? </p>`;
+popover.innerHTML = `<div class="title-popover">Popover title</div>  
+    <div class="content-popover">And here's some amazing content. It's very engaging. Right? </div>`;
+
 button.offsetParent.appendChild(popover);
-popover.style.bottom = `${button.offsetTop + 50}px`;
-popover.style.left = `${button.offsetTop + button.offsetHeight / 2}px`;
+popover.style.bottom = `${button.offsetTop + button.offsetHeight}px`;
+popover.style.left = `${button.offsetLeft - 8}px`;
 popover.style.display = 'none';
 
 button.addEventListener('click', () => {
